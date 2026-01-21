09f52e26588132ab22c8dd14e611929feb7f1180
C. Titus Brown <titus@idyll.org>
2026-01-21 14:23:09 -0800

    best change ever

--- summarize-csv.py
+++ summarize-csv.py
@@ -2,4 +2,4 @@ import sys
 import polars as pl
 
 df = pl.read_csv(sys.argv[1])
-print("Percent identified:", df['f_unique_weighted'].sum())
+print("Percent identified:", df['f_unique_weighted'].sum()*100)
